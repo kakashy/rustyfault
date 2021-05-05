37ed9e45930f34149261a814c743c9d1f3f11023
kakashy <josephwawerug@gmail.com>
2021-05-05 17:21:12 +0300

    Sesh Twice

--- src/strings.rs
+++ src/strings.rs
@@ -1,5 +1,20 @@
 pub fn run(){
-    let hello = String::from("ello");
+    let mut hello = String::from("Hello ");
+
+    // push char
+    hello.push('W');
+
+    // push a string
+    hello.push_str("orld");
+
+    // capacity
+    println!("Capacity: {} bytes", hello.capacity());
+
+    // replace
+    println!("Replacing: {}", hello.replace("World", "Darkness"));
+
+    //  contains
+println!("Contains 'World': {}", hello.contains("World"));
 
     println!("{}", hello)
 }
